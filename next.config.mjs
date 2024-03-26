bea337243c92ee70a8f70420644c3fcfdb8e5b7f
Juani gallo <ji.gallo93@gmail.com>
2024-03-26 10:09:11 -0300

    Base integration sdk with next

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+
+    return config;
+  },
 };
 
 export default nextConfig;
---
+++ pages/api/amplify/intent.js
@@ -0,0 +1,28 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from "axios";
+
+export default async function handler(req, res) {
+  if (req.method == "POST") {
+    try {
+      const { API_KEY, CLIENT_ID, BASE_URL } = process.env;
+
+      if (!API_KEY || !CLIENT_ID || !BASE_URL) {
+        throw new Error(
+          "API_KEY, CLIENT_ID or BASE_URL missing in env.local file"
+        );
+      }
+
+      const newPaymentIntent = await axios.post(
+        `${BASE_URL}/api/payment_intent`,
+        {
+          apiKey: process.env.API_KEY,
+          clientId: process.env.CLIENT_ID,
+        }
+      );
+      return res.status(200).json(newPaymentIntent.data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  res.status(200).json({ name: "John Doe" });
+}
